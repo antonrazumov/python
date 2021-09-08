059a06f27e35467a382bf345d10afbce1385dbc6
anton razumov <ntox23@gmail.com>
2021-09-08 15:00:22 +0300

    add the first python dz

---
+++ group_22.py
@@ -0,0 +1,41 @@
+p = 'anton'
+q = 1
+w = 3.75
+y = bytes (0x17)
+l = [1, 3, 56, 'xb']
+e = (4, 55, 39)
+r = set ('45')
+t = frozenset ('76')
+o = {'23' : 34}
+print(p, type(p))
+print(q, type(q))
+print(w, type(w))
+print(y, type(y))
+print(l, type(l))
+print(e, type(e))
+print(r, type(r))
+print(t, type(t))
+print(o, type(o))
+# print(type(p), type(q), type(w), type(y), type(l), type(e), type(r), type(t), type(o))
+# print (p, q, w, y, l, e, r, t, 0)
+
+
+qw = 'qwert'
+wq = 'trewq'
+er = qw + wq
+print (er, type(er))
+
+tr = 145
+rt = 56780
+yt = tr + rt
+u = rt / tr
+j = tr * rt
+k = rt // tr
+n = rt % tr
+print (yt, type(yt))
+print (u, type(u))
+print (j, type(j))
+print (k, type(k))
+print (n, type(n))
+
+
